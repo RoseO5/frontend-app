64c6ff8164c4cf45d6ee52642475d3c738d9ad9d
Rose05 <talktorose90@gmail.com>
2025-09-20 21:51:19 +0100

    Add my-image.png and update homepage to display it

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,20 @@
 import Hero from '../components/Hero'
 import Gallery from '../components/Gallery'
+import Image from 'next/image'
 
 export default function Home() {
   return (
     <main>
       <Hero />
       <Gallery />
+      <div className="flex justify-center mt-8">
+        <Image 
+          src="/my-image.png" 
+          alt="Project Screenshot" 
+          width={600} 
+          height={400} 
+        />
+      </div>
     </main>
   )
 }
